0f6eb88b5073bec2029ed55583f28fe362deaab2
KacperGodyn <kacper.godyn@microsoft.wsei.edu.pl>
2025-10-14 20:06:07 +0200

    another typo

--- frontend/my-expo-app/scripts/fix-paths.js
+++ frontend/my-expo-app/scripts/fix-paths.js
@@ -4,8 +4,8 @@ const path = require('path');
 const indexHtml = path.join(__dirname, '../dist/index.html');
 let content = fs.readFileSync(indexHtml, 'utf-8');
 
-content = content.replace(/href="\/_expo\//g, 'href="/swipevibes/_expo/');
-content = content.replace(/src="\/_expo\//g, 'src="/swipevibes/_expo/');
+content = content.replace(/href="\/_expo\//g, 'href="/SwipeVibes/_expo/');
+content = content.replace(/src="\/_expo\//g, 'src="/SwipeVibes/_expo/');
 
 fs.writeFileSync(indexHtml, content, 'utf-8');
 console.log('✓ Fixed paths');
